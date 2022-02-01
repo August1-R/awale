d7732373e1731d9c1b5c786ce0048caa3db0b18d
augustin <augustin.raimbault@gmail.com>
2022-02-01 19:59:37 +0100

    test fonction jouerCoup (non fonctionel)

--- main.py
+++ main.py
@@ -7,3 +7,31 @@ Le joueur True est le joueur qui a plateau[1] devant lui
 def initialisationPlateau():
     return [[4,4,4,4,4,4],[4,4,4,4,4,4]]
 
+def jouercoup(coup, plateau):
+    pierre = plateau[coup[0]][coup[1]]
+    plateau[coup[0]][coup[1]] = 0
+    ligne = coup[0]
+    colone = coup[1]
+    if colone + 1 <= 5:
+        colone += 1
+        if ligne + 1 <= 1:
+            colone = 0
+            ligne += 1
+        else:
+            colone = 0
+            ligne = 0
+        plateau[ligne][colone] += 1
+        pierre -= 1
+    gain = 0
+    while True:
+        if plateau[ligne][colone] <= 2:
+            gain = + plateau[ligne][colone]
+            plateau[ligne][colone] = 0
+            if colone - 1 >= 0:
+                colone -= 1
+            else:
+                return[plateau, gain]
+
+
+print(jouercoup([1,2],[[4,4,4,4,4,4]
+                        ,[4,4,4,4,4,4]]))
